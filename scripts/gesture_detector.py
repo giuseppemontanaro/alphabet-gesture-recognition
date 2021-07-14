f88bdea8cfb3a49a89e8df6a312a48a1fcf61e16
Simone-Smaldore <simone.smaldore@gmail.com>
2021-07-14 17:55:36 +0200

    ß Aggiunta strategia per sintetizzare le parole scritte con i sintetizzatore vocale

--- scripts/gesture_detector.py
+++ scripts/gesture_detector.py
@@ -3,6 +3,9 @@ import HandTrackingModule as htm
 import pickle
 import json
 import pandas as pd
+from collections import Counter
+from os import system
+
 
 from landmark_operator import get_landmark_distance_row
 
@@ -10,6 +13,7 @@ from landmark_operator import get_landmark_distance_row
 MODEL = pickle.load(open('../models/model.sav', 'rb'))
 ALPHABET = json.load(open('../json/alphabet_mapping.json', 'rb'))
 NUM_LANDMARK = 21
+NUM_PREDICTION_FRAME = 10
 
 
 def main():
@@ -22,6 +26,8 @@ def main():
 
 
 def detect(detector, cap):
+	buffer_letter = []
+	word_to_say = ''
 	while True:
 		success, img = cap.read()
 		img = detector.draw_hands_on_image(img)
@@ -33,9 +39,24 @@ def detect(detector, cap):
 			model_input = pd.DataFrame(data, index=[0])
 			probabilities = MODEL.predict_proba(model_input)[0]
 			pred_prob = max(probabilities)
-			if pred_prob > 0.7:
+			if pred_prob > 0.6:
 				prediction = MODEL.predict(model_input)[0]
+				buffer_letter.append(prediction)
 				print(f'pred: {ALPHABET[str(prediction)]}, with probability: {pred_prob}')
+			if len(buffer_letter) == NUM_PREDICTION_FRAME:
+				counter = Counter(buffer_letter).most_common(1) 
+				dict_counter = dict(counter)
+				value = list(dict_counter.values())[0]
+				key = list(dict_counter.keys())[0]
+				if(value > NUM_PREDICTION_FRAME*0.7):
+					if key == 16:
+						#word_to_say = word_to_say + ' '
+						system(f'say {word_to_say}')
+						word_to_say = ''
+					else:
+						word_to_say = word_to_say + ALPHABET[str(key)] 
+					print(word_to_say)
+				buffer_letter=[]
 
 
 def predict():
