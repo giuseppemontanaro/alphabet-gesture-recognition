15994ee20fa488b95adf00a492e34f3f14c64994
Simone-Smaldore <simone.smaldore@gmail.com>
2021-07-22 17:24:40 +0200

    ß Corretto bug nella valutazione del modello

--- scripts/test_cnn.py
+++ scripts/test_cnn.py
@@ -6,22 +6,22 @@ from tensorflow.keras.models import load_model
 
 
 def get_orthogonal_rect(image, points):
-    min_x = image.shape[1]
-    max_x = -1
-    min_y = image.shape[0]
-    max_y = -1
-    for point in points:
-        x = point[0]
-        y = point[1]
-        if x < min_x:
-            min_x = x
-        if x > max_x:
-            max_x = x
-        if y < min_y:
-            min_y = y
-        if y > max_y:
-            max_y = y
-    return int(min_x), int(max_x), int(min_y), int(max_y)
+	min_x = image.shape[1]
+	max_x = -1
+	min_y = image.shape[0]
+	max_y = -1
+	for point in points:
+		x = point[0]
+		y = point[1]
+		if x < min_x:
+			min_x = x
+		if x > max_x:
+			max_x = x
+		if y < min_y:
+			min_y = y
+		if y > max_y:
+			max_y = y
+	return int(min_x), int(max_x), int(min_y), int(max_y)
 
 
 #MODEL_CNN = load_model('../models/cnn_model_h_q.h5')
@@ -33,12 +33,12 @@ LANDMARK_MODEL_PATH = MODEL_HAND_TRACKING_PATH + "hand_landmark.tflite"
 ANCHORS_PATH = MODEL_HAND_TRACKING_PATH + "anchors.csv"
 
 detector = HandTracker(
-    False,
-    PALM_MODEL_PATH,
-    LANDMARK_MODEL_PATH,
-    ANCHORS_PATH,
-    box_shift=0.2,
-    box_enlarge=1
+	False,
+	PALM_MODEL_PATH,
+	LANDMARK_MODEL_PATH,
+	ANCHORS_PATH,
+	box_shift=0.2,
+	box_enlarge=1
 )
 
 
@@ -46,7 +46,8 @@ capture = cv.VideoCapture(0)
 wCam, hCam = 640, 320
 capture.set(3, wCam)
 capture.set(4, hCam)
-
+DEBUG = True
+cont = 0
 while True:
 	hasFrame, image = capture.read()
 	points, bbox = detector(image)
@@ -56,10 +57,16 @@ while True:
 		cropped = image[min_y : max_y-1, min_x : max_x-1]
 		if cropped.size != 0:
 			cropped = cv.resize(cropped, (150, 150))
+			if DEBUG:
+				img_name = str(cont)
+				cont = cont + 1
+				cv.imwrite(f'test/{img_name}.png', cropped)  
+			cropped = cv.cvtColor(cropped, cv.COLOR_RGB2BGR)
 			cropped = np.expand_dims(cropped, axis=0)
 			predictions = MODEL_CNN.predict(cropped)
 			idx = np.argmax(predictions)
 			print(predictions)
+
 			if idx == 0: print('h')
 			if idx == 1: print('p')
 			if idx == 2: print('q')
